49d6aa18537f2f493835ef4f7c113ceefd1e7092
Žiga Zupančič <zigazupancic5@gmail.com>
2017-05-09 16:28:37 +0200

    Dodaj modele za bazo (ORM).

--- getrecipe/models.py
+++ getrecipe/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+class Sestavina(models.Model):
+    ime = models.CharField(max_length=30)
+
+
+class Mera(models.Model):
+    ime = models.CharField(max_length=30)
+
+
+class TipJedi(models.Model):
+    ime = models.CharField(max_length=30)
+
+
+class Recept(models.Model):
+    ime = models.CharField(max_length=50)
+    ocena = models.FloatField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(10)])
+    povezava = models.URLField(max_length=200)
+    zahtevnost = models.FloatField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(10)])
+    cas_priprave = models.FloatField(validators=[validators.MinValueValidator(0)])
+    postopek = models.TextField(max_length=10000)
+    tipi = models.ManyToManyField(TipJedi)
+    sestavine = models.ManyToManyField(Sestavina, through='Priprava')
+
+
+class Priprava(models.Model):
+    kolicina = models.FloatField
+    mera = models.ForeignKey(Mera, on_delete=models.CASCADE)
+    sestavina = models.ForeignKey(Sestavina, on_delete=models.CASCADE)
+    recept = models.ForeignKey(Recept, on_delete=models.CASCADE)
