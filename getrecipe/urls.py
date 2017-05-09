120d9dbff63e65309afe3d3187cf5d6f9aa3e42d
Žiga Zupančič <zigazupancic5@gmail.com>
2017-05-09 21:28:50 +0200

    Dodaj all_recipes pogled za ogled vseh receptov.

--- getrecipe/urls.py
+++ getrecipe/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^detail/(?P<recipe_id>[0-9]+)/$', views.detail, name='detail')
+    url(r'^detail/(?P<recipe_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^all/', views.all_recipes, name='all_recipes')
 ]
--- getrecipe/views.py
+++ getrecipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Recept
 
 
@@ -9,3 +9,8 @@ def index(request):
 def detail(request, recipe_id):
     recipe = get_object_or_404(Recept, pk=recipe_id)
     return render(request, 'getrecipe/detail.html', {'recept': recipe})
+
+
+def all_recipes(request):
+    recipes = get_list_or_404(Recept)
+    return render(request, 'getrecipe/allrecipes.html', {'recepti': recipes})
