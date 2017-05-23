365af6f51ab8d09e345b4a3cd5cbaebae9144c71
Žiga Zupančič <zigazupancic5@gmail.com>
2017-05-23 20:13:30 +0200

    Popravi, da deluje tudi če v bazi ni sestavin.

--- getrecipe/forms.py
+++ getrecipe/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.shortcuts import get_list_or_404
 from .models import Sestavina
 
+
 class IngredientsForm(forms.Form):
-    available_ingredients = get_list_or_404(Sestavina)
+    available_ingredients = Sestavina.objects.all()
     choices = []
     for ingredient in available_ingredients:
         choices.append((ingredient.id, ingredient.ime))
