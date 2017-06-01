16f2d9d1218f7893315bdcb3da795871ddc78f07
Samo Kralj <samokralj95@gmail.com>
2017-06-01 19:50:11 +0200

    test csv

--- ZajemSpletneStrani.py
+++ ZajemSpletneStrani.py
@@ -79,7 +79,7 @@ def naredi_csv():
             ime_recepta = podatki['ime_recepta'][0]
             tezavnost = podatki['tezavnost'][0]
             cas_priprave = podatki['cas_priprave'][0]
-            line = '{0}, {1}, {2}, \n'.format(ime_recepta, tezavnost, cas_priprave)
+            line = '{0}; {1}; {2}; \n'.format(ime_recepta, tezavnost, cas_priprave)
             glavni_podatki_o_receptu.write(line)
 
             #Sestavine - zaenkrat dodamo samo vse sestavine
@@ -87,7 +87,7 @@ def naredi_csv():
                 k = kolicina
                 if 'label' in k:
                     k = ''
-                line = '{0}, {1}, {2}, \n'.format(ime_recepta, sestavina, k)
+                line = '{0}; {1}; {2}; \n'.format(ime_recepta, sestavina, k)
                 recepti_sestavine.write(line)
             ID += 1
             print(ID, ime_recepta)
