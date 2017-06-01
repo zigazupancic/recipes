a04bda6ad2a67fe44ff7932ef7ae8c957742adb6
Samo Kralj <samokralj95@gmail.com>
2017-06-01 19:38:12 +0200

    Testiram, če je csv lepo narjen

--- ZajemSpletneStrani.py
+++ ZajemSpletneStrani.py
@@ -60,10 +60,10 @@ def zajemi_podatke(url_recept):
 
 def naredi_csv():
     
-    glavni_podatki_o_receptu = open('glavni_recepti.txt', 'w')
+    glavni_podatki_o_receptu = open('glavni_recepti.csv', 'w')
     glavni_podatki_o_receptu.write('id, ime sestavine, tezavnost, cas_priprave \n')
     
-    recepti_sestavine = open('recepti-sestavine.txt', 'w')
+    recepti_sestavine = open('recepti-sestavine.csv', 'w')
     recepti_sestavine.write('recept, sestavina, količina \n')
     urls = open('url-naslovi-strani-receptov.txt', 'r')
 
